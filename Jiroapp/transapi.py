dbe9081ef96ea519272b0745fb575a66ecf64b6b
darkchii <906835882@qq.com>
2018-07-04 11:28:58 +0800

    Update transapi.py

--- Jiroapp/transapi.py
+++ Jiroapp/transapi.py
@@ -1,21 +1,6 @@
 # 提供翻译
 baidu_transapi = 'http://fanyi.baidu.com/transapi'
 
-# sug提供翻译单词
-baidu_sug_api = 'http://fanyi.baidu.com/sug'
-
-# 这个api需要验证签名 所以我基本不用它
-baidu_v2transapi = 'http://fanyi.baidu.com/v2transapi'
-
-# sug的 data post方式
-baidu_sug_data = {
-    'kw': 'string'
-}
-'''
-# 对应的输出格式
-print(html.json()['data'][0]['v'])
-'''
-
 # data post方式
 baidu_transapi_data = {
     'from': 'en',
@@ -27,3 +12,32 @@ baidu_transapi_data = {
 # 对应的输出格式
 print(html.json()['data'][0]['dst'])
 '''
+
+
+# sug提供翻译单词
+baidu_sug_api = 'http://fanyi.baidu.com/sug'
+
+# sug的 data post方式
+baidu_sug_data = {
+    'kw': 'string'
+}
+'''
+# 对应的输出格式
+print(html.json()['data'][0]['v'])
+'''
+
+
+# 这个api需要验证签名 所以基本不用它
+baidu_v2transapi = 'http://fanyi.baidu.com/v2transapi'
+
+# http://fy.iciba.com
+iciba_transapi = 'http://fy.iciba.com/ajax.php'
+
+# data get方式
+# 无需任何防爬策略
+iciba_transapi_data = {
+    'a': 'fy',
+    'f': 'auto',
+    't': 'auto',
+    'w': '测试'
+}
